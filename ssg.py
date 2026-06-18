6d1b28cfda00d13c70775b60b2d08394518e8f64
mewzok7@yahoo.com <mewzok7@yahoo.com>
2026-06-18 06:04:59 -0600

    Remove learning comments

--- ssg.py
+++ ssg.py
@@ -31,25 +31,3 @@ for file_path in input_folder.rglob("*.md"):
     output_path.write_text(output, encoding="utf-8")
 
     print(f"Generated: {output_path}")
-
-# file_path = "content/index.md"
-# output_folder = Path("site")
-# output_path = output_folder / "index.html"
-
-# # obtain both the frontmatter and split it out to obtain content
-# post = frontmatter.load(file_path)
-# title = str(post['title'])
-
-# # convert content into html
-# template = Path("template.html").read_text()
-
-# # inject content into template
-# output = template.replace("{{title}}", title).replace("{{content}}", post.content)
-
-# # convert markdown into html
-# html = markdown.markdown(output)
-
-# # create output folder if doesnt exist
-# output_folder.mkdir(parents=True, exist_ok=True)
-
-# output_path.write_text(html, encoding="utf-8")
